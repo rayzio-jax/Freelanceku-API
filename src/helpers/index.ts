6b3d3cc2570adb6dd89df36c94664d87880fc36c
rayhan <rayhananthaakbar@gmail.com>
2023-11-29 09:42:53 +0700

    add: password hashing

---
+++ src/helpers/index.ts
@@ -0,0 +1,11 @@
+import crypto from "crypto";
+
+const SECRET = "FREEJOB_API";
+
+export const random = () => crypto.randomBytes(128).toString("base64");
+export const auth = (salt: string, password: string) => {
+	return crypto
+		.createHmac("sha256", [salt, password].join("/"))
+		.update(SECRET)
+		.digest("hex");
+};
