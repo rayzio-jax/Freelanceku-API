5ef3962d4fd60a30145bec7884c559c33c97faad
rayhan <rayhananthaakbar@gmail.com>
2023-12-02 08:50:10 +0700

    update: delete user controller

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -30,3 +30,17 @@ export const getUsernameAndEmail = async (req: Request, res: Response) => {
 		return res.sendStatus(400);
 	}
 };
+
+export const deleteUser = async (req: Request, res: Response) => {
+	try {
+		const id = req.query.id as string;
+
+		const deletedUser = await deleteUserById(id);
+
+		// return res.json(deletedUser);
+		return response(200, deletedUser, "delete user: success", res);
+	} catch (error) {
+		console.log(error);
+		return res.sendStatus(400);
+	}
+};
