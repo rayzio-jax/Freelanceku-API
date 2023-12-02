128fc26d8d95e8a03b28640e0e96c63583bb4353
rayhan <rayhananthaakbar@gmail.com>
2023-12-02 09:15:07 +0700

    update: change error response

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -1,7 +1,8 @@
 import { Request, Response, NextFunction } from "express";
 import { get, merge } from "lodash";
+import { error } from "../response";
 
-import { getUserBySession, getUsers } from "../db/users";
+import { getUserBySession } from "../db/users";
 
 export const isAuthToDelete = async (
 	req: Request,
@@ -13,21 +14,17 @@ export const isAuthToDelete = async (
 		const currentUserId = get(req, "identity._id") as string;
 
 		if (!currentUserId) {
-			return res.status(403).send("Current user id not exist: Forbidden");
+			return error(403, "Current user id not exist: Forbidden", res);
 		}
 
-		const currentUsername = get(req, "identity.username") as string;
-
-		if (currentUsername !== "admin" && currentUserId.toString() !== id) {
-			return res
-				.status(403)
-				.send("User is not admin to perform special command: Forbidden");
+		if (currentUserId.toString() !== id) {
+			return error(403, "Unable to delete other user: Forbidden", res);
 		}
 
 		next();
 	} catch (error) {
 		console.log(error);
-		return res.sendStatus(400);
+		return error(400, "Bad request: invalid");
 	}
 };
 
@@ -39,18 +36,18 @@ export const isAuthenticated = async (
 	try {
 		const sessionToken = req.cookies["FREEJOB_API"];
 		if (!sessionToken) {
-			return res.sendStatus(403);
+			return error(403, "Session not authenticated: Forbidden", res);
 		}
 
 		const existingUser = await getUserBySession(sessionToken);
 		if (!existingUser) {
-			return res.sendStatus(403);
+			return error(403, "User not logged in: Forbidden", res);
 		}
 
 		merge(req, { identity: existingUser });
 		return next();
 	} catch (error) {
 		console.log(error);
-		return res.sendStatus(400);
+		return error(400, "Bad request: invalid");
 	}
 };
