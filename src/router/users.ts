da1a2dc1cdc218749905c701930ccb650b2feba0
rayhan <rayhananthaakbar@gmail.com>
2023-12-02 09:01:16 +0700

    update: patch user route

--- src/router/users.ts
+++ src/router/users.ts
@@ -4,11 +4,13 @@ import {
 	deleteUser,
 	getAllUsers,
 	getUsernameAndEmail,
+	updateUser,
 } from "../controllers/users";
 import { isAuthToDelete, isAuthenticated } from "../middlewares";
 
 export default (router: Router) => {
 	router.get("/users", getUsernameAndEmail);
 	router.get("/users/all", isAuthenticated, getAllUsers);
-	router.delete("/user", isAuthenticated, isAuthToDelete, deleteUser);
+	router.delete("/users", isAuthenticated, isAuthToDelete, deleteUser);
+	router.patch("/users", isAuthenticated, isAuthToDelete, updateUser);
 };
