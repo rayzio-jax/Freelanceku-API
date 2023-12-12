82d36b9a584a8ad64c49cd85980dd93d313c1078
rayhan <rayhananthaakbar@gmail.com>
2023-12-12 09:02:00 +0700

    update: add freelancers route & base route

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,18 @@
-import { Router } from "express";
+import { Router, Request, Response } from "express";
 import authentication from "./authentication";
 import users from "./users";
+import freelancers from "./freelancers";
 
 const router = Router();
+
+// Base route
+router.get("/", (req: Request, res: Response) => {
+	res.send("Welcome to my API");
+});
 
 export default (): Router => {
 	authentication(router);
 	users(router);
+	freelancers(router);
 	return router;
 };
