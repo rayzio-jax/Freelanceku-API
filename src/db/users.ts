27eb735badd7005da47157b7e6f17c3cb1a665aa
rayhan <rayhananthaakbar@gmail.com>
2023-12-12 09:01:45 +0700

    update: add timestamp create_at

--- src/db/users.ts
+++ src/db/users.ts
@@ -1,14 +1,17 @@
 import mongoose from "mongoose";
 
-const UserSchema = new mongoose.Schema({
-	username: { type: String, required: true },
-	email: { type: String, required: true },
-	authentication: {
-		password: { type: String, required: true, select: false },
-		salt: { type: String, select: false },
-		sessionToken: { type: String, select: false },
+const UserSchema = new mongoose.Schema(
+	{
+		username: { type: String, required: true },
+		email: { type: String, required: true },
+		authentication: {
+			password: { type: String, required: true, select: false },
+			salt: { type: String, select: false },
+			sessionToken: { type: String, select: false },
+		},
 	},
-});
+	{ timestamps: { createdAt: "created_at" } }
+);
 
 export const UserModel = mongoose.model("User", UserSchema);
 
