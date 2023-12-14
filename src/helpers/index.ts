6dba12b5ee7a6a377b60015378fba53b29391af5
rayhan <rayhananthaakbar@gmail.com>
2023-12-14 08:34:54 +0700

    update: use .env config

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,11 +1,12 @@
 import crypto from "crypto";
+import "dotenv/config";
 
-const SECRET = "FREEJOB_API";
+const TOKEN = process.env.SECRET_ACCESS_TOKEN;
 
 export const random = () => crypto.randomBytes(128).toString("base64");
 export const authentication = (salt: string, password: string) => {
 	return crypto
 		.createHmac("sha256", [salt, password].join("/"))
-		.update(SECRET)
+		.update(TOKEN)
 		.digest("hex");
 };
