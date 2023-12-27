9815c7aafba9f1229bf530788071bbcb44212dcc
rayhan <rayhananthaakbar@gmail.com>
2023-12-27 08:53:39 +0700

    update middleware to auth

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -1,8 +1,9 @@
+import "dotenv/config";
 import { Request, Response, NextFunction } from "express";
 import { get, merge } from "lodash";
 import { errorResponse } from "../response";
-
 import { getUserBySession } from "../db/users";
+import { verifyToken } from "../helpers";
 
 export const isAuthToDelete = async (
 	req: Request,
@@ -14,17 +15,27 @@ export const isAuthToDelete = async (
 		const currentUserId = get(req, "identity._id") as string;
 
 		if (!currentUserId) {
-			return errorResponse(403, "Current user id not exist: Forbidden", res);
+			return errorResponse(404, "NOT FOUND", "Current User Not Exist", res);
 		}
 
 		if (currentUserId.toString() !== id) {
-			return errorResponse(403, "Unable to delete other user: Forbidden", res);
+			return errorResponse(
+				403,
+				"FORBIDDEN",
+				"Unable To Delete Other User",
+				res
+			);
 		}
 
 		next();
 	} catch (error) {
 		console.log(error);
-		return errorResponse(400, "Bad request: invalid", res);
+		return errorResponse(
+			400,
+			"ERROR",
+			"Server Error: Failed To Authenticate",
+			res
+		);
 	}
 };
 
@@ -34,20 +45,49 @@ export const isAuthenticated = async (
 	next: NextFunction
 ) => {
 	try {
-		const sessionToken = req.cookies["FREEJOB_API"];
+		const sessionToken = req.cookies["SessionTokenId"];
+		const apiKey = req.headers.authorization;
+
 		if (!sessionToken) {
-			return errorResponse(403, "Session not authenticated: Forbidden", res);
+			return errorResponse(
+				401,
+				"UNAUTHORIZE",
+				"Session Not Authenticated",
+				res
+			);
 		}
 
-		const existingUser = await getUserBySession(sessionToken);
+		const existingUser = await getUserBySession(sessionToken).select(
+			"authentication.key"
+		);
+
 		if (!existingUser) {
-			return errorResponse(403, "User not logged in: Forbidden", res);
+			return errorResponse(400, "ERROR", "User doesn't exist", res);
+		}
+
+		const PUBLIC_KEY = process.env.PUBLIC_KEY;
+		const tokenData = verifyToken(sessionToken, PUBLIC_KEY, res) as {
+			_id: string;
+		};
+		const userId = tokenData._id;
+
+		if (!tokenData || userId !== existingUser._id.toString()) {
+			return errorResponse(401, "UNAUTHORIZE", "Session Expired", res);
+		}
+
+		if (!apiKey || apiKey !== existingUser.authentication.key) {
+			return errorResponse(401, "UNAUTHORIZE", `Invalid API Key`, res);
 		}
 
 		merge(req, { identity: existingUser });
-		return next();
+		next();
 	} catch (error) {
 		console.log(error);
-		return errorResponse(400, "Bad request: invalid", res);
+		return errorResponse(
+			400,
+			"ERROR",
+			"Server Error: Failed To Authenticate",
+			res
+		);
 	}
 };
