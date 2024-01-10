e0d9c134adca59049f23cf0a3aa553df7dcd8a7c
rayhan <rayhananthaakbar@gmail.com>
2024-01-10 15:17:56 +0700

    update: remove delete auth on patch

--- src/router/users.ts
+++ src/router/users.ts
@@ -12,5 +12,5 @@ export default (router: Router) => {
 	router.get("/v1/users", getAllUsernameAndEmail);
 	router.get("/v1/users/all", isAuthenticated, getAllUser);
 	router.delete("/v1/users", isAuthenticated, isAuthToDelete, deleteUser);
-	router.patch("/v1/users", isAuthenticated, isAuthToDelete, updateUser);
+	router.patch("/v1/users", isAuthenticated, updateUser);
 };
