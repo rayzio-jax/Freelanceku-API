2ee7a0ce6c75e2d3f6dc16f878a4a8ff88d9c934
rayhan <rayhananthaakbar@gmail.com>
2024-01-18 06:53:46 +0700

    update: freelancer update request

--- src/controllers/freelancers.ts
+++ src/controllers/freelancers.ts
@@ -4,7 +4,53 @@ import {
 	getFreelancerByEmail,
 	getFreelancers,
 	createFreelancer,
+	getFreelancerByUsername,
+	updateFreelancerByEmail,
 } from "../db/freelancers";
+
+export const updateFreelancer = async (req: Request, res: Response) => {
+	try {
+		const { username } = req.params;
+		const {
+			new_first_name,
+			new_last_name,
+			new_username,
+			new_email,
+			new_phone,
+			new_address,
+			new_province,
+			new_country,
+			new_description,
+		} = req.body;
+
+		const freelancer = await getFreelancerByUsername(username);
+		if (!freelancer)
+			return errorResponse(404, "NOT FOUND", "User not exist", res);
+
+		const newFreelancer = updateFreelancerByEmail(freelancer.email.toString(), {
+			first_name: new_first_name,
+			last_name: new_last_name,
+			username: new_username,
+			email: new_email,
+			phone: new_phone,
+			address: new_address,
+			province: new_province,
+			country: new_country,
+			description: new_description,
+		});
+
+		return response(
+			200,
+			"SUCCESS",
+			newFreelancer,
+			"Updating freelancer successful",
+			res
+		);
+	} catch (error) {
+		console.log(error);
+		return errorResponse(400, "ERROR", "Failed updating freelancer", res);
+	}
+};
 
 export const getAllFreelancer = async (req: Request, res: Response) => {
 	try {
@@ -76,9 +122,11 @@ export const registerFreelancer = async (req: Request, res: Response) => {
 			username,
 			email,
 			phone,
-			address,
-			province,
-			country,
+			local_address: {
+				address,
+				province,
+				country,
+			},
 			description,
 		});
 
@@ -88,9 +136,9 @@ export const registerFreelancer = async (req: Request, res: Response) => {
 			username: freelancer.username,
 			email: freelancer.email,
 			phone: freelancer.phone,
-			address: freelancer.address,
-			province: freelancer.province,
-			country: freelancer.country,
+			address: freelancer.local_address.address,
+			province: freelancer.local_address.province,
+			country: freelancer.local_address.country,
 			description: freelancer.description,
 		};
 
--- src/router/freelancers.ts
+++ src/router/freelancers.ts
@@ -3,6 +3,7 @@ import { Router } from "express";
 import {
 	getAllFreelancer,
 	registerFreelancer,
+	updateFreelancer,
 } from "../controllers/freelancers";
 import Validate from "../middlewares/validate";
 import { isAuthenticated } from "../middlewares";
@@ -61,5 +62,57 @@ export default (router: Router) => {
 		Validate,
 		isAuthenticated,
 		registerFreelancer
+	);
+	router.patch(
+		"/v1/freelancer/:username",
+		body("first_name")
+			.notEmpty()
+			.withMessage("First name is missing")
+			.isLength({ max: 30 })
+			.withMessage("First name max length is 30")
+			.trim()
+			.escape(),
+		body("last_name")
+			.notEmpty()
+			.withMessage("Last name is missing")
+			.isLength({ max: 30 })
+			.withMessage("Last name max length is 30")
+			.trim()
+			.escape(),
+		body("username")
+			.notEmpty()
+			.withMessage("Username is required")
+			.isLength({ max: 20 })
+			.withMessage("Username max length is 20")
+			.toLowerCase()
+			.trim()
+			.escape(),
+		body("email")
+			.notEmpty()
+			.withMessage("Email is required")
+			.isEmail()
+			.withMessage("Enter a valid email address")
+			.normalizeEmail(),
+		body("phone")
+			.notEmpty()
+			.withMessage("Phone number is required")
+			.isMobilePhone("any")
+			.withMessage("Enter a valid phone number"),
+		body("address").notEmpty().withMessage("Address is required").toLowerCase(),
+		body("province")
+			.notEmpty()
+			.withMessage("Province is required")
+			.toLowerCase(),
+		body("country").notEmpty().withMessage("Country is required").toLowerCase(),
+		body("description")
+			.notEmpty()
+			.withMessage("Freelancer bio is required")
+			.isString()
+			.withMessage("Freelancer bio must be string")
+			.trim()
+			.escape(),
+		Validate,
+		isAuthenticated,
+		updateFreelancer
 	);
 };
