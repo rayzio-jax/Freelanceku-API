35f693ba7eb4df4b093fbc7d51e6d380b6d76a33
rayhan <rayhananthaakbar@gmail.com>
2024-02-06 19:50:21 +0700

    feat: add new routes

--- src/router/transactions.ts
+++ src/router/transactions.ts
@@ -3,13 +3,45 @@ import { Router } from "express";
 import {
 	getAllTransaction,
 	createNewTransaction,
+	updateTransactionStatusByPaymentId,
+	updateTransactionStatusById,
 } from "../controllers/transactions";
 import Validate from "../middlewares/validate";
 import { isAuthenticated } from "../middlewares";
-import { body } from "express-validator";
+import { body, param } from "express-validator";
 
 export default (router: Router) => {
-	router.get("/v1/transaction", isAuthenticated, getAllTransaction);
+	router.patch(
+		"/v1/transaction/id/:_id",
+		param("_id").notEmpty().withMessage("Transaction ID required as parameter"),
+		body("new_status")
+			.isIn(["UNPROCESSED", "FAILED", "PENDING", "DONE"])
+			.withMessage("Status must be one of: UNPROCESSED, FAILED, PENDING, DONE")
+			.toUpperCase()
+			.isString()
+			.withMessage("New status must be string")
+			.notEmpty()
+			.withMessage("Transaction new status is required"),
+		isAuthenticated,
+		updateTransactionStatusById
+	);
+	router.patch(
+		"/v1/transaction/payment_id/:payment_id",
+		param("payment_id")
+			.notEmpty()
+			.withMessage("Transaction payment ID required as parameter"),
+		body("new_status")
+			.isIn(["UNPROCESSED", "FAILED", "PENDING", "DONE"])
+			.withMessage("Status must be one of: UNPROCESSED, FAILED, PENDING, DONE")
+			.toUpperCase()
+			.isString()
+			.withMessage("New status must be string")
+			.notEmpty()
+			.withMessage("Transaction new status is required"),
+		isAuthenticated,
+		updateTransactionStatusByPaymentId
+	);
+	router.get("/v1/transactions", isAuthenticated, getAllTransaction);
 	router.post(
 		"/v1/transaction/new",
 		body("payment_id").notEmpty().withMessage("Payment ID is missing"),
@@ -20,9 +52,9 @@ export default (router: Router) => {
 			.isEmail({ host_blacklist: ["yopmail.com"] })
 			.withMessage("Enter a valid email address"),
 		body("amount")
-			.isLength({ min: 4 })
-			.withMessage("Minimum amount is 50.000")
 			.toInt()
+			.isInt({ min: 50000 })
+			.withMessage("Minimum amount is 50.000")
 			.isNumeric()
 			.withMessage("Amount must be number, not string")
 			.notEmpty()
@@ -33,6 +65,8 @@ export default (router: Router) => {
 			.notEmpty()
 			.withMessage("Transaction message is required"),
 		body("status")
+			.isIn(["UNPROCESSED", "FAILED", "PENDING", "DONE"])
+			.withMessage("Status must be one of: UNPROCESSED, FAILED, PENDING, DONE")
 			.toUpperCase()
 			.escape()
 			.trim()
