e032adc562d71a6fd6eb3d43b4fd32a1b537f18a
rayhan <rayhananthaakbar@gmail.com>
2024-02-11 11:41:48 +0700

    refactor: validation position

--- src/router/authentication.ts
+++ src/router/authentication.ts
@@ -8,28 +8,26 @@ export default (router: Router) => {
 	router.post(
 		"/v1/auth/register",
 		body("first_name")
-			.optional({ values: "null" })
+			.trim()
+			.escape()
 			.isLength({ max: 30 })
 			.withMessage("First name max length is 30")
 			.notEmpty()
-			.withMessage("First name must be not empty")
-			.trim()
-			.escape(),
+			.withMessage("First name is required"),
 		body("last_name")
-			.optional({ values: "null" })
+			.trim()
+			.escape()
 			.isLength({ max: 30 })
 			.withMessage("Last name max length is 30")
 			.notEmpty()
-			.withMessage("Last name must be not empty")
-			.trim()
-			.escape(),
+			.withMessage("Last name is required"),
 		body("username")
+			.trim()
+			.escape()
 			.isLength({ max: 20 })
 			.withMessage("Username max length is 20")
 			.notEmpty()
-			.withMessage("Username is required")
-			.trim()
-			.escape(),
+			.withMessage("Username is required"),
 		body("email")
 			.normalizeEmail({
 				gmail_remove_dots: false,
@@ -37,7 +35,20 @@ export default (router: Router) => {
 			.isEmail({ host_blacklist: ["yopmail.com"] })
 			.withMessage("Enter a valid email address"),
 		body("password")
-			.isStrongPassword()
+			.isStrongPassword({
+				minLength: 8, // Minimum length . Default is 8 characters
+				minLowercase: 1, // Minimum lowercase letters. Default is 1
+				minUppercase: 1, // Minimum uppercase letters. Default is 1
+				minNumbers: 1, // Minimum numbers. Default is 1
+				minSymbols: 1, // Minimum symbols. Default is 1
+				returnScore: false, // When enabled, it will return the actual score. Default is false
+				pointsPerUnique: 1, // Points for every unique character. Default is 1
+				pointsPerRepeat: 0.5, // Points for every repeated character. Default is 0.5
+				pointsForContainingLower: 10, // Points for containing lowercase letters. Default is 10
+				pointsForContainingUpper: 10, // Points for containing uppercase letters. Default is 10
+				pointsForContainingNumber: 10, // Points for containing numbers. Default is 10
+				pointsForContainingSymbol: 10, // Points for containing symbols. Default is 10
+			})
 			.withMessage("Password is weak")
 			.isLength({ min: 8 })
 			.withMessage("Password atleast 8 chars long")
