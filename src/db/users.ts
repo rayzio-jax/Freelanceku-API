2baa4c5306a22ae20fac32c603f8a485e8a0260f
rayhan <rayhananthaakbar@gmail.com>
2024-03-04 22:11:24 +0700

    change typo

--- src/db/users.ts
+++ src/db/users.ts
@@ -337,7 +337,7 @@ export const updateUserByEmail = async (
 			values.identity.first_name || user.identity.first_name;
 		values.identity.last_name =
 			values.identity.last_name || user.identity.last_name;
-		values.identity.usename =
+		values.identity.username =
 			values.identity.username || user.identity.username;
 		values.identity.email = values.identity.email || user.identity.email;
 		values.identity.phone = values.identity.phone || user.identity.phone;
