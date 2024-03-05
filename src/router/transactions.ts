7de43b79f752791cdb46ccbfe95292839e92f632
rayhan <rayhananthaakbar@gmail.com>
2024-03-05 07:23:31 +0700

    feat: update, delete transaction

--- src/router/transactions.ts
+++ src/router/transactions.ts
@@ -3,34 +3,31 @@ import { Router } from "express";
 import {
 	getAllTransaction,
 	createNewTransaction,
-	updateTransactionStatus,
+	updateTransByIdOrPayId,
+	getTransByIdOrPaymentId,
+	deleteTransByIdOrPaymentId,
 } from "../controllers/transactions";
 import Validate from "../middlewares/validate";
 import { isAdmin, isAuthenticated } from "../middlewares";
 import { body, param, query } from "express-validator";
 
 export default (router: Router) => {
-	router.patch(
-		"/v1/transaction/status/:_id",
-		param("_id")
-			.notEmpty()
-			.withMessage("Transaction ID or Payment ID is required as parameter"),
-		body("new_status")
-			.isIn(["UNPROCESSED", "FAILED", "PENDING", "DONE"])
-			.withMessage("Status must be one of: UNPROCESSED, FAILED, PENDING, DONE")
-			.toUpperCase()
-			.isString()
-			.withMessage("New status must be string")
-			.notEmpty()
-			.withMessage("Transaction new status is required"),
+	router.delete("/v1/transaction/:_id"),
+		param("_id").notEmpty().withMessage("Invalid or missing parameter"),
 		Validate,
 		isAuthenticated,
-		updateTransactionStatus
-	);
+		isAdmin,
+		deleteTransByIdOrPaymentId;
+	router.get("/v1/transaction/:_id");
+	param("_id").notEmpty().withMessage("Invalid or missing parameter"),
+		Validate,
+		isAuthenticated,
+		isAdmin,
+		getTransByIdOrPaymentId;
 	router.get(
 		"/v1/transactions",
-		query("size").notEmpty().withMessage("Size is required"),
-		query("page").notEmpty().withMessage("Page is required"),
+		query("size").notEmpty().withMessage("Param 'size' is required"),
+		query("page").notEmpty().withMessage("Param 'page' is required"),
 		Validate,
 		isAuthenticated,
 		isAdmin,
@@ -69,5 +66,54 @@ export default (router: Router) => {
 		Validate,
 		isAuthenticated,
 		createNewTransaction
+	);
+	router.patch(
+		"/v1/transaction/:_id",
+		param("_id")
+			.notEmpty()
+			.withMessage("Transaction ID or Payment ID is required as parameter"),
+		body("new_payment_id")
+			.optional({ values: "falsy" })
+			.trim()
+			.escape()
+			.isString()
+			.withMessage("Invalid new payment id format, only accept string"),
+		body("new_sender_id")
+			.optional({ values: "falsy" })
+			.trim()
+			.escape()
+			.isString()
+			.withMessage("Invalid new sender id format, only accept string"),
+		body("new_receiver_id")
+			.optional({ values: "falsy" })
+			.trim()
+			.escape()
+			.isString()
+			.withMessage("Invalid new receiver id format, only accept string"),
+		body("new_amount")
+			.optional({ values: "falsy" })
+			.trim()
+			.escape()
+			.isNumeric()
+			.withMessage("Invalid new amount format, only accept number/integer"),
+		body("new_message")
+			.optional({ values: "falsy" })
+			.trim()
+			.escape()
+			.isString()
+			.withMessage("Invalid new message format, only accept string"),
+		body("new_status")
+			.optional({ values: "falsy" })
+			.trim()
+			.escape()
+			.toUpperCase()
+			.isIn(["UNPROCESSED", "FAILED", "PENDING", "DONE"])
+			.withMessage("Status must be one of: UNPROCESSED, FAILED, PENDING, DONE")
+			.isString()
+			.withMessage("New status must be string"),
+		Validate,
+		isAuthenticated,
+		isAdmin,
+		updateTransByIdOrPayId
 	);
 };
